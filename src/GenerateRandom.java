cf0674cb9adf01fa74b8d9fedd7639c7f3131a1d
Prakash K <kprakash19940625@gmail.com>
2017-11-01 18:09:11 +0530

    Update GenerateRandom.java

--- src/GenerateRandom.java
+++ src/GenerateRandom.java
@@ -1,4 +1,6 @@
 import java.util.Random;
+import java.io.*;
+import java.util.*;
 public class GenerateRandom {
   public static int generate() {
     Random randomGenerator = new Random();
@@ -6,7 +8,18 @@ public class GenerateRandom {
     return n;
   }
   public static void main(String args[]){
-    System.out.println("Hello World");
+    //System.out.println("Hello World");
+    File file = new File("index.html");
+    FileWriter writer;
+    try {
+        writer = new FileWriter(file, true);
+        PrintWriter printer = new PrintWriter(writer);
+        printer.write("HelloWorld");
+        printer.close();
+    } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
     //System.out.println(generate());
   }
 }
