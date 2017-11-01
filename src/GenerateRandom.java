1b3414e53b749743c88db71a990f97fdc69501c7
Prakash K <kprakash19940625@gmail.com>
2017-11-01 16:18:02 +0530

    Update GenerateRandom.java

--- src/GenerateRandom.java
+++ src/GenerateRandom.java
@@ -6,6 +6,7 @@ public class GenerateRandom {
     return n;
   }
   public static void main(String args[]){
-    System.out.println(generate());
+    System.out.println("Hello World");
+    //System.out.println(generate());
   }
 }
